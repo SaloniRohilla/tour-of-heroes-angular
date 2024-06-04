7c49f3b7d89340023dc3bca82b67ccc33d24c41c
SaloniRohilla <83385669+SaloniRohilla@users.noreply.github.com>
2024-06-04 20:54:30 +0530

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Saloni' },
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Superman' },
+  { id: 15, name: 'Tarzan' },
+  { id: 16, name: 'Wonder Woman' },
+  { id: 17, name: 'Hercules' },
+  { id: 18, name: 'Hulk' },
+  { id: 19, name: 'Robin Hood' },
+  { id: 20, name: 'Tornado' }
+];
